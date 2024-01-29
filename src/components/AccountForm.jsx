745e246e85b6c9caaadced3345f39b1ef1c507e8
mat-afk <mateusc.ramos@outlook.com>
2024-01-29 17:08:37 -0300

    feat: Account Form component

--- src/components/AccountForm.jsx
+++ src/components/AccountForm.jsx
@@ -1,5 +1,25 @@
 const AccountForm = () => {
-  return <div>AccountForm</div>;
+  return (
+    <div>
+      <div className="form-control">
+        <label>
+          <span>Name:</span>
+          <input type="text" id="name" placeholder="Enter your name" required />
+        </label>
+      </div>
+      <div className="form-control">
+        <label>
+          <span>Email:</span>
+          <input
+            type="email"
+            id="email"
+            placeholder="Enter your email"
+            required
+          />
+        </label>
+      </div>
+    </div>
+  );
 };
 
 export default AccountForm;
