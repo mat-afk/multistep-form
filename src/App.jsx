46f61fc57ef845b1b5f3dbafbdf505ead8718bd8
mat-afk <mateusc.ramos@outlook.com>
2024-01-29 15:05:21 -0300

    feat: App component static elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,35 @@
+// Components
+import { GrFormNext, GrFormPrevious } from "react-icons/gr";
+
+// CSS
 import "./App.css";
 
 function App() {
   return (
-    <div>
-      <h1>Multistep form</h1>
+    <div className="app">
+      <div className="header">
+        <h2>Share your review</h2>
+        <p>
+          We are happy with your purchase, use the form below to rate the
+          product.
+        </p>
+      </div>
+      <div className="form-container">
+        <p>steps</p>
+        <form>
+          <div className="inputs-container"></div>
+          <div className="actions">
+            <button type="button">
+              <GrFormPrevious />
+              <span>Back</span>
+            </button>
+            <button type="submit">
+              <span>Next</span>
+              <GrFormNext />
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
